706d66912ddf222940cade139ba45f8d2db9ae7d
ValeriaERodriguez <valeriaelisabetrgz@gmail.com>
2022-05-10 21:11:54 -0300

    oraganizacion

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const inputTexto = document.querySelector(".input-text");
-const mensaje = document.querySelector(".input-tex-area");
+const inputTexto = document.querySelector('.input-texto');
+const mensaje = document.querySelector('.texto-result');
 
 function botonEncriptar(){
+    const textoEncriptado = encriptar(inputText.value);
+    mensaje.value = textoEncriptado;
+}
+
+function encriptar(StringParaEncriptar){
     
-};
+    let matrizCodigo =[["e","enter"],["i","imes"],["a","ai"],["o","ober"],["u","ufat"]]
+    StringParaEncriptar = StringParaEncriptar.toLowerCase();
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if(StringParaEncriptar.includes(matrizCodigo[i][0])){
+            StringParaEncriptar = StringParaEncriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+        }
+    }
+    return StringParaEncriptar;
+}
