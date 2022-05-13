0c8c116c32140d111364ad1dcb4c73547801fdba
ValeriaERodriguez <valeriaelisabetrgz@gmail.com>
2022-05-12 22:04:46 -0300

    Modificaciones

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const inputTexto = document.querySelector('.input-texto');
 const mensaje = document.querySelector('.texto-result');
+const botonCopiar = document.getElementById('.copiar');
 
 function botonEncriptar(){
     const textoEncriptado = encriptar(inputTexto.value);
@@ -18,3 +19,48 @@ function encriptar(StringParaEncriptar){
     return StringParaEncriptar;
 }
 
+function botonDesencriptar(){
+    const textoDesencriptado = desencriptar(inputTexto.value);
+    mensaje.value = textoDesencriptado
+
+}
+function desencriptar(StringParaEncriptar){
+    StringParaEncriptar=StringParaEncriptar.replaceAll("enter","e");
+    StringParaEncriptar=StringParaEncriptar.replaceAll("imes","i");
+    StringParaEncriptar=StringParaEncriptar.replaceAll("ai","a");
+    StringParaEncriptar=StringParaEncriptar.replaceAll("ober","o");
+    StringParaEncriptar=StringParaEncriptar.replaceAll("ufat","u");
+
+    return StringParaEncriptar;
+
+}
+
+// function copiar(){
+//     const copiarTexto = mensaje.value;
+//     navigator.clipboard.writeText(copiarTexto.value)
+
+//         if (copiarTexto){
+//             alert("No hay texto que copiar");
+//         } else {
+//             alert("Texto copiado a portapapeles");
+//         }
+    
+// }
+
+// botonCopiar.addEventListener('click',function (_copiar) {
+//         Event.preventDefault();
+//         var mensajeEncriptado = mensaje.value;
+//         navigator.clipboard.writeText(mensajeEncriptado);
+
+//         copiarAlPortapapeles(mensajeEncriptado);
+//   //  })
+
+function copiar(){
+    var content = document.getElementById('copiar').innerHTML;
+    navigator.clipboard.writeText(content)
+    .then(() => {
+        console.log("Texto copiado al portapapeles")
+        alert("Copiado")
+    })
+
+}
