a0bcfe3fff9ea45c5e20ef10cfc1917ec83f4c7b
ValeriaERodriguez <valeriaelisabetrgz@gmail.com>
2022-05-24 22:08:08 -0300

    .

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const inputTexto = document.querySelector('.input-texto');
 const mensaje = document.querySelector('.texto-result');
 const botonCopiar = document.getElementById('.copiar');
+var figura = document.getElementById('.figura');
+var contenedor = document.getElementById('.cont');
 
 
 function botonEncriptar(){
+   
+   
     const textoEncriptado = encriptar(inputTexto.value);
     mensaje.value = textoEncriptado;
+
+    if (inputTexto.value == "") {
+        figura.style.visibility = "visible";
+        mensaje.style.visibility = "visible";
+        botonCopiar.style.visibility = "hidden";
+    } else {
+        figura.style.visibility = "hidden";
+        mensaje.style.visibility = "hidden";
+        mensaje.style.visibility = "hidden";
+        
+        inputTexto.value = "";
+        inputTexto.focus();
+    }
+
+    
 }
 
 function encriptar(StringParaEncriptar){
@@ -17,6 +36,7 @@ function encriptar(StringParaEncriptar){
             StringParaEncriptar = StringParaEncriptar.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
         }
     }
+    
     return StringParaEncriptar;
 }
 
@@ -26,6 +46,10 @@ function botonDesencriptar(){
 
 }
 function desencriptar(StringParaEncriptar){
+
+    
+    
+
     StringParaEncriptar=StringParaEncriptar.replaceAll("enter","e");
     StringParaEncriptar=StringParaEncriptar.replaceAll("imes","i");
     StringParaEncriptar=StringParaEncriptar.replaceAll("ai","a");
@@ -34,53 +58,62 @@ function desencriptar(StringParaEncriptar){
 
     return StringParaEncriptar;
 
+   
+
 }
 
-// function copiar(){
-//     const copiarTexto = mensaje.value;
-//     navigator.clipboard.writeText(copiarTexto.value)
 
-//         if (copiarTexto){
-//             alert("No hay texto que copiar");
-//         } else {
-//             alert("Texto copiado a portapapeles");
-//         }
-    
-// }
-
-// botonCopiar.addEventListener('click',function (_copiar) {
-//         Event.preventDefault();
-//         var mensajeEncriptado = mensaje.value;
-//         navigator.clipboard.writeText(mensajeEncriptado);
-
-//         copiarAlPortapapeles(mensajeEncriptado);
-//   //  })
-
-// function copiar(){
-//     var content = document.getElementById('copiar').innerHTML;
-//     navigator.clipboard.writeText(content)
-//     .then(() => {
-//         console.log("Texto copiado al portapapeles")
-//         alert("Copiado")
-//     })
-
-// }
-
-function copiar() {
-    var contenido = document.querySelector("#texto-result");
-    contenido.select();
-    document.execCommand("copy");
-    alert("!Se copió correctamente!");
-  }
-
-function copy() {
-
-    var btnMensaje = document.getElementById("texto-result").innerHTML;
-    document.getElementById("input-texto").value = btnMensaje;
+ function copiar(){
+    const copiarTexto = mensaje.value;
     mensaje.value = "";
-    navigator.clipboard.writeText(content)
-    
-    
-    
+    navigator.clipboard.writeText(copiarTexto)
 
+        copiarAlPortapapeles(copiarTexto);
+        document.execCommand(commandId='copy');
+
+        const boton3 = document.getElementById("btncopiar");
+        boton3.onclick = copiar;
+    
+ }
+
+ 
+
+
+
+function ocultarAdelante(){
+    figura.classList.add("ocultar");
+    h3.classList.add("ocultar");
+    h4.classList.add("ocultar");
 }
+
+/*
+
+function ocultarSalida(){ 
+    figura.classList.add("ocultar");
+    contenedor.classList.add("ocultar");
+    inputTexto.classList.add("ocultar");
+    mensaje.removeAttribute("hidden");
+  }
+  function copiarAlPortapapeles(id_elemento) {
+
+    // Crea un campo de texto "oculto"
+    var aux = document.createElement("input");
+  
+    // Asigna el contenido del elemento especificado al valor del campo
+    aux.setAttribute("value", document.getElementById(id_elemento).innerHTML);
+  
+    // Añade el campo a la página
+    document.body.appendChild(aux);
+  
+    // Selecciona el contenido del campo
+    aux.select();
+  
+    // Copia el texto seleccionado
+    document.execCommand("copy");
+  
+    // Elimina el campo de la página
+    document.body.removeChild(aux);
+  
+  }*/
+
+  
